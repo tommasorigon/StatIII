c00ff2d41f9e891cdcbc0a5f43ffdcc7404e8b58
Tommaso Rigon <tommaso.rigon@gmail.com>
2025-08-01 13:50:47 +0200

    asdasd

--- code/un_B.R
+++ code/un_B.R
@@ -16,7 +16,39 @@ ggplot(data = Beetles, aes(x = logdose, y = deaths / n)) +
   geom_point() +
   geom_function(fun = function(x) plogis(coef(fit_Beetles)[1] + coef(fit_Beetles)[2] * x), linetype = "dashed") +
   theme_light() +
-  theme(legend.position = "right") +
-  scale_color_tableau(palette = "Color Blind") +
   xlab("log-dose") +
   ylab("Proportion of deaths")
+
+fit_Beetles_lm <- lm(I(deaths / n) ~ logdose, data = Beetles)
+fit_Beetles_logit <- lm(qlogis((deaths + 0.5) / (n + 1)) ~ logdose, data = Beetles)
+
+ggplot(data = Beetles, aes(x = logdose, y = deaths / n)) +
+  xlim(c(1.65, 1.92)) +
+  geom_point() +
+  geom_function(fun = function(x) plogis(coef(fit_Beetles)[1] + coef(fit_Beetles)[2] * x), linetype = "dashed") +
+  geom_function(fun = function(x) coef(fit_Beetles_lm)[1] + coef(fit_Beetles_lm)[2] * x, linetype = "dashed", col = "darkorange") +
+  geom_function(fun = function(x) plogis(coef(fit_Beetles_logit)[1] + coef(fit_Beetles_logit)[2] * x), linetype = "dashed", col = "darkblue") +
+  theme_light() +
+  xlab("log-dose") +
+  ylab("Proportion of deaths")
+
+data(Aids)
+colnames(Aids) <- c("deaths", "period")
+rownames(Aids) <- paste(1983:1986, rep(1:4, each = 4), sep = "-")[-c(15:16)]
+knitr::kable(t(Aids[1:7, ]))
+knitr::kable(t(Aids[8:14, ]))
+
+ggplot(data = Aids, aes(x = period, y = deaths)) +
+  geom_point() +
+  theme_light() +
+  xlab("Period") +
+  ylab("Deaths")
+
+fit_Aids <- glm(deaths ~ period, family = "poisson", data = Aids)
+
+ggplot(data = Aids, aes(x = period, y = deaths)) +
+  geom_point() +
+  geom_function(fun = function(x) exp(coef(fit_Aids)[1] + coef(fit_Aids)[2] * x), linetype = "dashed") +
+  theme_light() +
+  xlab("Period") +
+  ylab("Deaths")
