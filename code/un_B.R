f3e2a67be3876b1d1db8342a60317b402c2eb690
Tommaso Rigon <tommaso.rigon@gmail.com>
2025-08-01 11:12:28 +0200

    asdasd

--- code/un_B.R
+++ code/un_B.R
@@ -9,3 +9,14 @@ ggplot(data = Beetles, aes(x = logdose, y = deaths / n)) +
   scale_color_tableau(palette = "Color Blind") +
   xlab("log-dose") +
   ylab("Proportion of deaths")
+
+fit_Beetles <- glm(cbind(deaths, n - deaths) ~ logdose, family = "binomial", data = Beetles)
+
+ggplot(data = Beetles, aes(x = logdose, y = deaths / n)) +
+  geom_point() +
+  geom_function(fun = function(x) plogis(coef(fit_Beetles)[1] + coef(fit_Beetles)[2] * x), linetype = "dashed") +
+  theme_light() +
+  theme(legend.position = "right") +
+  scale_color_tableau(palette = "Color Blind") +
+  xlab("log-dose") +
+  ylab("Proportion of deaths")
