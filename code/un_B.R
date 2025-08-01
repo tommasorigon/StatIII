b1b25bb829690bc0782cb095de79325b222062b0
Tommaso Rigon <tommaso.rigon@gmail.com>
2025-08-01 20:29:19 +0200

    asdasd

--- code/un_B.R
+++ code/un_B.R
@@ -5,8 +5,6 @@ knitr::kable(Beetles)
 ggplot(data = Beetles, aes(x = logdose, y = deaths / n)) +
   geom_point() +
   theme_light() +
-  theme(legend.position = "right") +
-  scale_color_tableau(palette = "Color Blind") +
   xlab("log-dose") +
   ylab("Proportion of deaths")
 
