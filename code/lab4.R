35178b9c6268d1641df789408ec6968b87ee05a8
Tommaso Rigon <tommaso.rigon@gmail.com>
2025-10-22 12:10:50 +0200

    asdasd

--- code/lab4.R
+++ code/lab4.R
@@ -15,7 +15,7 @@ data("Beetles10")
 
 # QUESTION 1. The Beetles data can be "ungrouped". Fit a logistic regression model and verify that the maximum likelihood estimates and the standard errors coincide in the two representations. Verify that deviance and residuals do NOT coincide.
 
-# QUESTION 2. Perform a goodness of fit test against the saturated model.
+# QUESTION 2. Compare the predictions with the observed values. Perform a goodness of fit test against the saturated model.
 
 # QUESTION 3. Obtain a Wald confidence interval for the quantity \beta_1 + \beta_2 x, with x = 1.8. Compare you results with those obtained using the function "predict". Then, compute a confidence interval for the logistic transform of the above quantity (i.e., for the probability of dying when x = 1.8).
 
@@ -35,6 +35,21 @@ summary(m1_grouped)
 
 m1_ungrouped <- glm(ucciso ~ log.dose10, data = Beetles10, family = binomial(link = "logit"))
 summary(m1_ungrouped)
+
+# COMMENT: the maximum likelihood estimates and the standard errors coincide.
+
+deviance(m1_grouped)
+deviance(m1_ungrouped)
+
+# The residuals do NOT coincide. The number of observations is actually different
+residuals(m1_grouped)
+residuals(m1_ungrouped)
+
+# The first plot is meaningful and informative, the second one is not
+plot(fitted(m1_grouped), rstandard(m1_grouped))
+plot(fitted(m1_ungrouped), rstandard(m1_ungrouped))
+
+# QUESTION 2
 
 # -------------------------------------------------------------------
 # Dataset 2: Credit scoring - APPLIED ANALYSIS
